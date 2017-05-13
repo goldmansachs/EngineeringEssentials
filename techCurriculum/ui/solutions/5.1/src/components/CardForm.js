aec4a1c3e0f8ab65751c821b4a7cbc1480b4f5d7
tadas412 <tadas412@gmail.com>
2017-05-13 17:16:08 -0400

    br in wrong place

--- techCurriculum/ui/solutions/5.1/src/components/CardForm.js
+++ techCurriculum/ui/solutions/5.1/src/components/CardForm.js
@@ -52,6 +52,7 @@ class CardForm extends React.Component {
         <TextInput name='username' label='Username' value={this.state.username} onChange={this.handleUsernameChange}/>
         <TextInput name='message' label='Message' value={this.state.message} onChange={this.handleMessageChange}/>
         <Select options={tagOptions} multi/>
+        <br />
         <button className='btn btn-primary' onClick={this.handleSubmit}>Submit</button>
       </form>
     );
--- techCurriculum/ui/solutions/5.2/src/components/CardForm.js
+++ techCurriculum/ui/solutions/5.2/src/components/CardForm.js
@@ -56,8 +56,8 @@ class CardForm extends React.Component {
         <h2>Add a Card</h2>
         <TextInput name='username' label='Username' value={this.state.username} onChange={this.handleUsernameChange}/>
         <TextInput name='message' label='Message' value={this.state.message} onChange={this.handleMessageChange}/>
-        <br />
         <Select options={tagOptions} multi value={this.state.tags} onChange={this.handleTagsChange}/>
+        <br />
         <button className='btn btn-primary' onClick={this.handleSubmit}>Submit</button>
       </form>
     );
