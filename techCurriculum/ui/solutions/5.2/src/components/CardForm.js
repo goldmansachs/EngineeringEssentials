ff8644b4ac1e9022c910225e5cc5fd40e2ef7bd4
tadas412 <tadas412@gmail.com>
2017-05-13 17:14:04 -0400

    added br for tag button

--- techCurriculum/ui/solutions/5.2/src/components/CardForm.js
+++ techCurriculum/ui/solutions/5.2/src/components/CardForm.js
@@ -56,6 +56,7 @@ class CardForm extends React.Component {
         <h2>Add a Card</h2>
         <TextInput name='username' label='Username' value={this.state.username} onChange={this.handleUsernameChange}/>
         <TextInput name='message' label='Message' value={this.state.message} onChange={this.handleMessageChange}/>
+        <br />
         <Select options={tagOptions} multi value={this.state.tags} onChange={this.handleTagsChange}/>
         <button className='btn btn-primary' onClick={this.handleSubmit}>Submit</button>
       </form>
