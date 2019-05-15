9de6bbe9c39701b290c3c882b257cbd9633e6006
Elia Anagnostou <eliaanagnostou@Konstantinoss-MacBook-Air.local>
2019-05-15 11:07:34 -0400

    company class

--- caseStudy/services/src/main/java/pojo/Company.java
+++ caseStudy/services/src/main/java/pojo/Company.java
@@ -16,11 +16,85 @@
 
 package pojo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 /**
  * This class will define a company and its attributes
  * Look at resources/data/companyInfo.json
  */
 public class Company {
+    @JsonProperty
+    private String symbol;
+    @JsonProperty
+    private String name;
+    @JsonProperty
+    private String headquartersCity;
+    @JsonProperty
+    private String headquartersStateOrCountry;
+    @JsonProperty
+    private int numberOfEmployees;
+    @JsonProperty
+    private String sector;
+    @JsonProperty
+    private String industry;
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getHeadquartersCity() {
+        return headquartersCity;
+    }
+
+    public void setHeadquartersCity(String headquartersCity) {
+        this.headquartersCity = headquartersCity;
+    }
+
+    public String getHeadquartersStateOrCountry() {
+        return headquartersStateOrCountry;
+    }
+
+    public void setHeadquartersStateOrCountry(String headquartersStateOrCountry) {
+        this.headquartersStateOrCountry = headquartersStateOrCountry;
+    }
+
+    public int getNumberOfEmployees() {
+        return numberOfEmployees;
+    }
+
+    public void setNumberOfEmployees(int numberOfEmployees) {
+        this.numberOfEmployees = numberOfEmployees;
+    }
+
+    public String getSector() {
+        return sector;
+    }
+
+    public void setSector(String sector) {
+        this.sector = sector;
+    }
+
+    public String getIndustry() {
+        return industry;
+    }
+
+    public void setIndustry(String industry) {
+        this.industry = industry;
+    }
+
 
     // TODO - Think back to your modelling session
     // Define the attributes of a Company based on the
