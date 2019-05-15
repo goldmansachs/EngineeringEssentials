ea6e8e52ba76649eaa1409537f6f859047f4805f
kimjuyounglisa <39270614+kimjuyounglisa@users.noreply.github.com>
2019-05-15 15:58:37 -0400

    Update CompanyResource.java

--- caseStudy/services/src/main/java/resources/CompanyResource.java
+++ caseStudy/services/src/main/java/resources/CompanyResource.java
@@ -15,34 +15,55 @@
  */
 
 package resources;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
+
 import pojo.Company;
+import pojo.Stock;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.ListIterator;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
-import resources.FileHelper;
+import java.util.ArrayList;
+import java.util.SortedMap;
+import java.text.ParseException;
 
 import jdk.nashorn.internal.objects.annotations.Getter;
 
-    // Your service should return data for a given stock ticker
-
-@Path("companies")
+// TODO - add your @Path here
+@Path("company")
 public class CompanyResource {
 
-    private static final ObjectMapper mapper = new ObjectMapper();
+    // TODO - Add a @GET resource to get company data
+    // Your service should return data for a given stock ticker
+
+    private ObjectMapper mapper = new ObjectMapper();
 
     @GET
-    public Response getCompanyForTicker(String ticker) throws IOException {
+    public Response getByTicker(String ticker) throws IOException {
+        //TODO: Return the list of all of the events in the events.json file
+        List<Companiy> companies; // something Claire makes
 
-        List<Company> companies = FileHelper.readAllCompanies("companyInfo.json");
-
-        // Need to get .json object
-        //Company company; //get the company for ticker
-        //return Response.ok().entity(company).build();
-        return Response.ok().entity(companies).build();
+        for (Company c : companies) {
+            if (c.getSymbol() == ticker) {
+                return Response.ok().entity(c).build();
+            }
+        }
+        return Response.noContent().build();
     }
+
 
 }
