21e13bba89639b531f5ae6914373c417335aad35
Boubacar Diallo <boubacar.diallo1@macaulay.cuny.edu>
2019-05-15 08:56:55 -0400

    curriculum practice

--- techCurriculum/services/EngineeringEssentialsServices/src/main/java/examples/Example2Resource.java
+++ techCurriculum/services/EngineeringEssentialsServices/src/main/java/examples/Example2Resource.java
@@ -4,6 +4,8 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.io.IOException;
 
 /**
  * Copyright 2018 Goldman Sachs.
@@ -33,6 +35,6 @@ public class Example2Resource {
     @Path("test")
     @Produces(MediaType.APPLICATION_JSON)
     public String helloWorld() {
-        return "Hello, world!";
+        return "Welcome to Engineering Essentials Services Training!";
     }
 }
