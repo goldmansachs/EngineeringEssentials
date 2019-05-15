42e4ff480937fc351d0b5baaa4031fd6e0f8cf6c
Eno Reyes <enoreyes@gmail.com>
2019-05-15 12:45:10 -0400

    Updated CompanyResource

--- caseStudy/services/src/main/java/resources/CompanyResource.java
+++ caseStudy/services/src/main/java/resources/CompanyResource.java
@@ -16,26 +16,44 @@
 
 package resources;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.type.TypeReference;
+import pojo.Company;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
-import pojo.Company;
+import java.util.List;
+import java.io.File;
+import java.io.FileInputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
     // Your service should return data for a given stock ticker
 
 @Path("companies")
 public class CompanyResource {
 
+    private static final ObjectMapper mapper = new ObjectMapper();
+
     @GET
     public Response getCompanyForTicker(String ticker) throws IOException {
 
+        List<Company> companies = readAllCompanies();
+
         // Need to get .json object
-        Company company; //
-        return Response.ok().entity(company).build();
+        //Company company; //get the company for ticker
+        //return Response.ok().entity(company).build();
 
     }
 
+    public static List<Company> readAllCompanies() throws IOException {
+
+        InputStream inputStream = new FileInputStream(("./resources/data" + File.separatorChar + "companyInfo.json"));
+        return mapper.readValue(inputStream, new TypeReference<List<Company>>() {
+        });
+    }
 }
